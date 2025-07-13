fed4b2f04a46c26764fec71f4eacee531eef3a95
Parvesh <kumarparvesh49032@gmail.com>
2025-07-13 10:43:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.logo span').addEventListener('click', function() {
+        alert('Welcome to Pulpy Maid!');
+    }); 
+    document.querySelector('.btn').addEventListener('click', function() {
+        alert('Searching for services...');
+    });
+   
